ec4295860587187bb6a4d31a2697d465087457cd
Xianda Yu <176912231@163.com>
2016-04-28 23:27:57 +0800

    Update ReverseVowels.py

--- ReverseVowels.py
+++ ReverseVowels.py
@@ -7,11 +7,16 @@ Given s = "hello", return "holle".
 Example 2:
 Given s = "leetcode", return "leotcede".
 
+Analysis:
+1. record all vowels in string;
+2. revserse the record vowel list;
+3. make a new groups again by reverse vowel list.
 """
 
 class Solution(object):
     """
-    Result: Time Limit Exceeded 
+    Result: Time Limit Exceeded
+    Reason: use a bubble sort method to move vowels, slow!!!!! 
     """
     def reverseVowels(self, s):
         r = []
@@ -24,4 +29,25 @@ class Solution(object):
                 for j in range(len(vowel_index) - 1, 0, -1):
                     r[vowel_index[j]], r[vowel_index[j - 1]] = \
                         r[vowel_index[j - 1]], r[vowel_index[j]]
+        return ''.join(r)
+        
+        
+class Solution(object):
+    """
+    Result: Accept
+    """
+    def reverseVowels(self, s):
+        r = []
+        vowels = set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])
+        vowel_index = []
+        for i in range(len(s)):
+            if s[i] in vowels:
+                vowel_index.append(i)
+        last = len(vowel_index) - 1
+        for i in range(len(s)):
+            if s[i] in vowels:
+                r.append(s[vowel_index[last]])
+                last -= 1
+            else:
+                r.append(s[i])
         return ''.join(r)
