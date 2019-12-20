eb7ece598afca2f53f8389582bbdc4e09153631a
yuyu <176912231@163.com>
2019-12-20 13:54:39 +0800

    Update and rename 3Sum.cpp to 3sum.cpp

--- 3sum.cpp
+++ 3sum.cpp
@@ -1,4 +1,5 @@
 /*
+https://leetcode.com/problems/3sum/
 Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
 
 Note:
