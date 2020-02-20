1184604e5a3e4c2cb0981bb169f453dacbab26bd
simyy <xianda.yxd@gmail.com>
2020-02-20 14:11:55 +0800

    Rename count-and-say and New Python Solution

---
+++ count-and-say.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+"""
+https://leetcode.com/problems/count-and-say/
+
+The count-and-say sequence is the sequence of integers with the first five terms as following:
+1.     1
+2.     11
+3.     21
+4.     1211
+5.     111221
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count-and-say sequence.
+You can do so recursively, in other words from the previous member read off the digits,
+counting the number of digits in groups of the same digit.
+
+Note: Each term of the sequence of integers will be represented as a string.
+
+
+Example 1:
+Input: 1
+Output: "1"
+Explanation: This is the base case.
+
+Example 2:
+Input: 4
+Output: "1211"
+Explanation: For n = 3 the term was "21" in which we have two groups "2" and "1", "2" can be read as "12" which means frequency = 1 and value = 2, the same way "1" is read as "11", so the answer is the concatenation of "12" and "11" which is "1211".
+"""
+
+
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        pre_char_list = []
+        for i in range(1, n + 1):
+            pre_char_list = self.calc_on_pre(i, n, pre_char_list)
+        return "".join([str(_) for _ in pre_char_list])
+
+    def calc_on_pre(self, i, n, pre_char_list):
+        if i == 1:
+            return [1]
+        if i > n:
+            return pre_char_list
+        char_list = []
+        dup_count = 1
+        for i in range(len(pre_char_list)):
+            if i < len(pre_char_list) - 1 and pre_char_list[i] == pre_char_list[i + 1]:
+                dup_count += 1
+                continue
+            char_list.append(dup_count)
+            char_list.append(pre_char_list[i])
+            dup_count = 1
+        return char_list
+
+
+
+
+if __name__ == '__main__':
+    print Solution().countAndSay(1)
+    print Solution().countAndSay(2)
+    print Solution().countAndSay(3)
+    print Solution().countAndSay(4)
